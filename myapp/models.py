f4267ab8415209d770cb3312cb373e6ba1fb1e85
Bartlomiej Jakubowski <enshamrat@gmail.com>
2020-06-04 18:03:46 +0200

    added models and admin interface

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Author(models.Model):
+	first_name = models.CharField(max_length=50)
+	surname = models.CharField(max_length=100)
+
+	def __str__(self):
+		return "{} {}".format(self.surname, self.first_name)
+
+
+class Book(models.Model):
+	title = models.CharField(max_length=200)
+	pages = models.IntegerField()
+	author = models.ForeignKey(Author, on_delete=models.CASCADE)
+
+	def __str__(self):
+		return "{}".format(self.title)
