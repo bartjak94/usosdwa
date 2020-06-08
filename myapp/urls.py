69dba1932e11eb713dfde1906cb8d333b9d7dbe6
Bartlomiej Jakubowski <enshamrat@gmail.com>
2020-06-08 20:02:56 +0200

    books to student first step\

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('', views.index),
-	path('books',views.books_index, name='books-index'),
-	path('books/<int:book_id>', views.book_detail, name='books-details'),
-	path('books/add', views.book_add, name='books-add')
+	path('', views.index),  ##edupa cycki zrob tu logowanie i guziczki
+	path('lista_studentow',views.students_index, name='students-index'),
+	path('usosdwa/<int:book_id>', views.book_detail, name='books-details'),
+	path('usosdwa/add', views.book_add, name='books-add')
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -3,12 +3,20 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from django.urls import reverse
 
+from .models import Student, Subject
 from .models import Book
 from .forms import BookForm
 # Create your views here.
 
 def index(request):
-	return HttpResponse("<h1>USOS 2.0 very primitive model</h1>")
+	template = loader.get_template("myapp/base.html")
+	return render(request, "myapp/base.html")
+
+def students_index(request):
+	template = loader.get_template("myapp/index.html")
+	context = {"students": Student.objects.all()}
+	return HttpResponse(template.render(context, request))
+	
 
 def books_index(request):
 	template = loader.get_template("myapp/index.html")
@@ -29,3 +37,4 @@ def book_add(request):
 	else:
 		form = BookForm()
 		return render(request, "myapp/book_add.html", {'form': form})
+
