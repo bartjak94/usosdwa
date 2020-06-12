94e56d5d6625173692bf0ded3e4841944a0cb659
Kamil14210 <66635413+Kamil14210@users.noreply.github.com>
2020-06-12 20:16:45 +0200

    KKn 1

--- myapp/urls.py
+++ myapp/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
 	path('student/<int:student_id>', views.student_details),
 	path('subjects_index',views.subjects_index, name='subjects-index'),
 	path('subject_add', views.subject_add, name='subject-add'),
+    path('logg', views.log_in, name='login'),
 	path('subject/<int:subject_id>', views.subject_details)
 ]
