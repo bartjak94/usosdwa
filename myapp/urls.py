28d644500c6b5b42d8735845e2ef126627a766b0
bartjak94 <enshamrat@gmail.com>
2020-06-12 14:16:31 +0200

    added subjects with corresponding actions: add, show list, show details

--- myapp/urls.py
+++ myapp/urls.py
@@ -4,7 +4,9 @@ from . import views
 urlpatterns = [
 	path('', views.index, name='main-menu'),  ##edupa cycki zrob tu logowanie i guziczki
 	path('lista_studentow',views.students_index, name='students-index'),
-	#path('usosdwa/<int:book_id>', views.book_detail, name='books-details'),
 	path('add', views.student_add, name='student-add'),
-	path('student/<int:student_id>', views.student_details)
+	path('student/<int:student_id>', views.student_details),
+	path('subjects_index',views.subjects_index, name='subjects-index'),
+	path('subject_add', views.subject_add, name='subject-add'),
+	path('subject/<int:subject_id>', views.subject_details)
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -6,6 +6,7 @@ from django.urls import reverse
 from .models import Student, Subject
 from .models import Book
 from .forms import StudentForm
+from .forms import SubjectForm
 # Create your views here.
 
 def index(request):
@@ -30,4 +31,24 @@ def student_add(request):
 
 def student_details(request, student_id):
 	student = Student.objects.get(pk=student_id)
-	return render(request, "myapp/student_details.html", {"student":student})
+	return render(request, "myapp/student_details.html", {"student": student})
+
+def subjects_index(request):
+    template = loader.get_template("myapp/subjects_index.html")
+    context = {"subjects":Subject.objects.all()}
+    return HttpResponse(template.render(context, request))
+
+def subject_add(request):
+    if request.method == 'POST':
+        form = SubjectForm(request.POST)
+        if(form.is_valid()):
+            form.save()
+            return HttpResponseRedirect(reverse('subjects-index'))
+    else:
+        form = SubjectForm()
+        return render(request, "myapp/subject_add.html", {'form': form})
+
+def subject_details(request, subject_id):
+    subject = Subject.objects.get(pk=subject_id)
+    return render(request, "myapp/subject_details.html", {"subject": subject})
+
